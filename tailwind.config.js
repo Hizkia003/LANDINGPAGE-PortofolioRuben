801c5a0758c1ef29cad5de5edbd30d21f8a15290
Ruben <rubenhizkia03@gmail.com>
2025-10-02 02:28:58 +0700

    Inisialisasi project Tailwind dan update bg video

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#84cc16',
-        secondary: '#64748b',
-        dark: '#020617',
+        primary: "#84cc16",
+        secondary: "#64748b",
+        third: "#2b4207",
+        dark: "#020617",
       },
       screens: {
-        '2xl': '1320px',
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
 };
-
